0e740dbc9269142912bad22b16adc939b84668f9
krypton-byte <rosid6434@gmail.com>
2022-05-14 12:00:46 +0700

    GraphQL

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from FBPy.FBPy import Facebook
-fb = Facebook('ku.txt')
-import random
+from concurrent.futures import ThreadPoolExecutor
+from FBPy.FBPy import Facebook, FacebookAPI
+from itertools import count as ct
+import zlib
+from os import urandom
+fb = FacebookAPI()
+fb.login('','')
+fb.dumps(open('kue.txt', 'w'))
+print('dump')
+x = 0
 print(fb.get_name)
-print(fb.new_message([], "Hello World", []))
+print(fb.GraphQL.test().json())
+# print(fb.GraphQL.reactions('100050866593572_510920537280162', 'PRIDE').json())
